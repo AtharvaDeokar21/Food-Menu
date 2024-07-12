f53cd128c0831e64cc0a6537310b8eea22bd4458
AtharvaDeokar21 <atharva.deokar2005@gmail.com>
2024-07-12 22:17:08 +0530

    Connected user with each profile model

--- food/models.py
+++ food/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
     item_image = models.CharField(max_length=500, default="https://www.kindpng.com/picc/m/79-798754_hoteles-y-centros-vacacionales-dish-placeholder-hd-png.png")
+    
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
     
     
     def __str__(self) -> str:
